1e55d1b68afc053fc0406d1a2536ad89f6495749
jloaiz16 <jloaiz16@eafit.edu.co>
2018-02-23 10:03:19 -0500

    Modificando rutas para despliegue DCA

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  # Defino pagina inicial
-  root 'welcome#index'
-
-  # Defino rutas de las localizaciones
-  resources :locations
-  
-  # Defino ruta de peticion
-  get 'welcome/index'
-
-  # Rutas para usuarios
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
-  # Rutas para sesiones
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  scope '/proyecto1' do
+    # Defino pagina inicial
+    root 'welcome#index'
+    # Defino rutas de las localizaciones
+    resources :locations
+    # Defino ruta de peticion
+    get 'welcome/index'
+    # Rutas para usuarios
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    # Rutas para sesiones
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+  end
 end
